c1d7f586b6b0f8adf7417c6e228c6460ae2b4bcf
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 17:29:30 +0200

    Add cityinput

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Cities from './components/Cities';
+import CityInput from './components/CityInput';
 // import UilReact from '@iconscout/react-unicons/icons/uil-react';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <Cities />
+      <CityInput />
     </div>
 
   );
---
+++ src/components/CityInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CityInput() {
+  return (
+    <div>CityInput</div>
+  )
+}
+
+export default CityInput
