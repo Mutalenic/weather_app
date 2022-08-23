753c281a987a95a168c679a5a0fb1f9e9874f5e5
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 17:46:16 +0200

    Add input field

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+// import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
 
 function CityInput() {
   return (
-    <div>CityInput</div>
-  )
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row justify-center my-6">
+        <input type="text" className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize" />
+      </div>
+    </div>
+  );
 }
 
-export default CityInput
+export default CityInput;
