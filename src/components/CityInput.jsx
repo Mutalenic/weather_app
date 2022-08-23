849bcf7b282af1204f3d4f3249dee7f72ee9525f
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 17:50:24 +0200

    Add search placeholder to input

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -5,7 +5,7 @@ function CityInput() {
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row justify-center my-6">
-        <input type="text" className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize" />
+        <input type="text" placeholder="Search For City..." className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" />
       </div>
     </div>
   );
