08c2603c0dbf10a0929b82f07379cdd36101f999
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 16:59:31 +0200

    Add Array of objects

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
 
 function Cities() {
+    const places = [
+        {
+            id:1,
+            title:'Lusaka'
+        },
+        {
+            id:2,
+            title:'Nairobi'
+        },
+        {
+            id:3,
+            title:'Sydney'
+        },
+        {
+            id:4,
+            title:'New York'
+        }
+    ]
   return <div>Cities</div>;
 }
 
