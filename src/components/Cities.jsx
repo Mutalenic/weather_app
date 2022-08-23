b24f5ef64515d8128f565f95399b8ba074931b4f
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 16:53:10 +0200

    Fix linter errors

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 function Cities() {
-  return (
-    <div>Cities</div>
-  )
+  return <div>Cities</div>;
 }
 
-export default Cities
+export default Cities;
