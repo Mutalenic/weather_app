f5067512725ed2dac90bc9e19c563cfe2368d998
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 17:13:11 +0200

    Iterate over cities

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
 function Cities() {
-    const places = [
-        {
-            id:1,
-            title:'Lusaka'
-        },
-        {
-            id:2,
-            title:'Nairobi'
-        },
-        {
-            id:3,
-            title:'Sydney'
-        },
-        {
-            id:4,
-            title:'New York'
-        }
-    ]
-  return <div>Cities</div>;
+  const places = [
+    {
+      id: 1,
+      title: 'Lusaka',
+    },
+    {
+      id: 2,
+      title: 'Nairobi',
+    },
+    {
+      id: 3,
+      title: 'Sydney',
+    },
+    {
+      id: 4,
+      title: 'New York',
+    },
+  ];
+  return (
+    <div className="flex items-center justify-around my-6">
+      {places.map((place) => (
+        <button key={place.id} type="button">{place.title}</button>
+      ))}
+    </div>
+  );
 }
 
 export default Cities;
