0dddc55612b79c4b47bc9b59905dbc397910d4d0
Nicholas Mutale <nicomutale@gmail.com>
2022-08-23 20:52:30 +0200

    Add degree fahrenheit and celsius symbols

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -5,9 +5,36 @@ function CityInput() {
   return (
     <div className="flex flex-row justify-center items-center my-6">
       <div className="flex flex-row justify-center items-center my-6">
-        <input type="text" placeholder="Search For City..." className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" />
-        <UilSearch size={27} className="text-white cursor-pointer transition ease-out hover:scale-150 ml-2" />
-        <UilLocationPoint size={27} className="text-white cursor-pointer transition ease-out hover:scale-150" />
+        <input
+          type="text"
+          placeholder="Search For City..."
+          className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+        />
+        <UilSearch
+          size={27}
+          className="text-white cursor-pointer transition ease-out hover:scale-150 ml-2"
+        />
+        <UilLocationPoint
+          size={27}
+          className="text-white cursor-pointer transition ease-out hover:scale-150"
+        />
+      </div>
+      <div className="flex flex-row w-1/4 items-center justify-center">
+        <button
+          name="metric"
+          className="text-xl text-white font-light"
+          type="button"
+        >
+          °C
+        </button>
+        <p className="text-xl text-white mx-1">|</p>
+        <button
+          type="button"
+          name="imperial"
+          className="text-xl text-white font-light transition ease-out hover:125"
+        >
+          °F
+        </button>
       </div>
     </div>
   );
