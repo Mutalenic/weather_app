51d98d8a02698f7445bdfa11ca684ef9e63c50ae
Nicholas Mutale <nicomutale@gmail.com>
2022-08-24 21:20:39 +0200

    Add image to weather parameter

--- src/components/WeatherParameters.jsx
+++ src/components/WeatherParameters.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import sun from '../assets/sun.svg';
 
 function WeatherParameters() {
   return (
-    <div>WeatherParameters</div>
+    <div>
+      <div className="flex items-center justify-center my-6 text-3xl text-[rgb(150,255,2)] font-semibold">
+        <p>Windy</p>
+      </div>
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img src={sun} alt="sun" className="w-1/12" />
+      </div>
+    </div>
   );
 }
 
