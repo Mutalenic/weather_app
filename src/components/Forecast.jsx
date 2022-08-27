2ec6d0f8adfd352863e3d6fa7214a2f73cc9422f
Nicholas Mutale <nicomutale@gmail.com>
2022-08-27 16:50:30 +0200

    Add hourly forecast data

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import sun from '../assets/sun.svg';
 
 function Forecast() {
-  return <div>forecast</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-start my-6">
+        <p className="text-white font-medium uppercase">Hourly ForeCast</p>
+      </div>
+      <hr className="my-2" />
+      <div className="flex flex-row items-center justify-between text-white">
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">05:00PM</p>
+          <img src={sun} alt="sun" className="w-1/12" />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">05:00PM</p>
+          <img src={sun} alt="sun" className="w-1/12" />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">05:00PM</p>
+          <img src={sun} alt="sun" className="w-1/12" />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">05:00PM</p>
+          <img src={sun} alt="sun" className="w-1/12" />
+          <p className="font-medium">22°</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-light text-sm">05:00PM</p>
+          <img src={sun} alt="sun" className="w-1/12" />
+          <p className="font-medium">22°</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Forecast;
