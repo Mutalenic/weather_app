e307bada666a611f4453f9f8e9e7ace13f0b42a1
Nicholas Mutale <nicomutale@gmail.com>
2022-08-27 16:07:09 +0200

    Add weather parameters

--- src/components/WeatherParameters.jsx
+++ src/components/WeatherParameters.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import sun from '../assets/sun.svg';
-
+import {
+  UiArrowUp,
+  UilArrowDown,
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from '@iconscout/react-unicons';
 function WeatherParameters() {
   return (
     <div>
@@ -9,6 +17,25 @@ function WeatherParameters() {
       </div>
       <div className="flex flex-row items-center justify-between text-white py-3">
         <img src={sun} alt="sun" className="w-1/12" />
+        <p className="flex flex-col space-y-2">25°</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1"/>
+            Real feel:
+            <span className="font-medium ml-1">30°</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1"/>
+            Humidity:
+            <span className="font-medium ml-1">70%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1"/>
+            Wind Speed
+            <span className="font-medium ml-1">10 km/h</span>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
