bb9e6bae0bfe4e11872b14fc0cb282c3957165cf
Nicholas Mutale <nicomutale@gmail.com>
2022-08-30 23:25:30 +0200

    Destructure current weather data

--- src/components/services/weatherService.js
+++ src/components/services/weatherService.js
@@ -1,3 +1,5 @@
+import { data } from "autoprefixer";
+
 const API_KEY = '6574f405463f1e3a64b32c567ddd4bc8';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
@@ -5,8 +7,19 @@ const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + '/' + infoType);// eslint-disable-line prefer-template
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-  console.log(url);
-  return fetch(url).then((response) => response.json()).then((data) => data);
+  return fetch(url).then((response) => response.json())
 };
 
-export default getWeatherData;
+const formatCurrentWeatherData = (data) => {
+  const {
+    coord: {lat, lon},
+    main: {temp, feels_like, temp_min, temp_max, humidity},
+    name,
+    dt,
+    sys: {country, sunrise, sunset},
+    weather,
+    wind: {speed}
+  } = data
+
+  return {lat, lon, temp, humidity, weather, speed, name, dt, country, sunrise, sunset,temp_min, temp_max, feels_like}
+}
