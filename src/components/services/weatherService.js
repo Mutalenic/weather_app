eca872d0a29cb79d0805ed1648fb4a6304bdf9d0
mutalenic <nicomutale@gmail.com>
2022-10-02 21:40:02 +0200

    Update weather app

--- src/components/services/weatherService.js
+++ src/components/services/weatherService.js
@@ -1,5 +1,7 @@
 // import { data } from "autoprefixer";
 
+import { DateTime } from 'luxon';
+
 const API_KEY = '6574f405463f1e3a64b32c567ddd4bc8';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
@@ -49,22 +51,46 @@ const formatCurrentWeatherData = (data) => {
   };
 };
 
+const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
 const formatForecastWeather = (data) => {
-  let { timezone, daily, hourly} = data;
-  daily = daily.slice(1,6).map()
-}
+  const { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map(d =>
+     {
+    return ({
+      title: formatToLocalTime(d.dt, timezone, 'ccc'),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    });
+  });
+
+  hourly = hourly.slice(1, 6).map(d => {
+    return ({
+      title: formatToLocalTime(d.dt, timezone, 'ccc'),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    });
+  });
+
+  return {
+    timezone,
+    daily,
+    hourly,
+  };
+};
 
 const getFormattedWeatherData = async (searchParams) => {
   const currentWeatherData = await getWeatherData(
     'weather',
     searchParams,
   ).then(formatCurrentWeatherData);
+
   const { lat, lon } = currentWeatherData;
 
   const formattedForecastWeatherData = await getWeatherData('onecall', {
     lat, lon, exclude: 'current,minutely,alerts', units: searchParams.units,
   }).then(formatForecastWeather);
-  return currentWeatherData;
+  return { ...currentWeatherData, ...formattedForecastWeatherData };
 };
 
 export default getFormattedWeatherData;
